98ea420c1e6c1450b8f79a992a9c00ffa6f43e42
gmakaliana <134883961+gmakaliana@users.noreply.github.com>
2024-01-05 11:40:21 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/* Script 1  */
+
+function myFunction() {
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+/* Script 2  */
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+
+
+
